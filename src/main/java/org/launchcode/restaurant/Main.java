477628122fb2da32fe216564f3f7921744e2f981
varti2023 <varti.pant@gmail.com>
2023-08-29 17:07:10 -0500

    Adding restaurant-menu files

---
+++ src/main/java/org/launchcode/restaurant/Main.java
@@ -0,0 +1,44 @@
+package org.launchcode.restaurant;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Date;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+	// write your code here
+      //  ArrayList<MenuItem> menuItems = new ArrayList<>();
+        Menu menu = new Menu(LocalDate.now(), new ArrayList<MenuItem>());
+
+        menu.getItems().add(new MenuItem(10,"Pizza", "Main Course",false));
+        menu.getItems().add(new MenuItem(5,"Fritters", "Appetizer",true));
+        menu.getItems().add(new MenuItem(10,"Ice cream", "Dessert",true));
+        menu.getItems().add(new MenuItem(10,"Veggies", "Appetizer",false));
+
+        menu.addItems(new MenuItem(15,"Caramel Custard", "Dessert",true));
+        menu.addItems(new MenuItem(10,"Burrito Bowl", "Dinner",true));
+
+        System.out.println("\nItems in menu are:");
+        for(MenuItem items : menu.getItems()) {
+            System.out.println(items.getDescription());
+        }
+
+       menu.removeItems(new MenuItem(10,"Pizza", "Main Course",false));
+        System.out.println("\nItems in menu are:");
+        for(MenuItem items : menu.getItems()) {
+            System.out.println(items.getDescription());
+        }
+
+        System.out.println("\nNew items in menu are:");
+        for(MenuItem items : menu.getItems()) {
+            if(items.getNew()) {
+                System.out.println(items.getDescription());
+            }
+        }
+
+        System.out.println("\nMenu last updates on :" + menu.getLastUpdated());
+        }
+
+}
---
+++ src/main/java/org/launchcode/restaurant/Menu.java
@@ -0,0 +1,38 @@
+package org.launchcode.restaurant;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Date;
+
+public class Menu {
+    private LocalDate lastUpdated;
+    private ArrayList<MenuItem> items;
+
+    public Menu(LocalDate d, ArrayList<MenuItem> i) {
+        this.lastUpdated = d;
+        this.items = i;
+    }
+
+    public void setLastUpdated(LocalDate lastUpdated) {
+        this.lastUpdated = lastUpdated;
+    }
+
+    public void setItems(ArrayList<MenuItem> items) {
+        this.items = items;
+    }
+
+    public LocalDate getLastUpdated() {
+        return lastUpdated;
+    }
+
+    public ArrayList<MenuItem> getItems() {
+        return items;
+    }
+
+    public void addItems(MenuItem menuItem){
+        items.add(menuItem);
+    }
+    public void removeItems(MenuItem menuItem){
+        items.remove(menuItem);
+    }
+}
